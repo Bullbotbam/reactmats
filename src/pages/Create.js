aa910f7071a910000f9b4c01d96a7a04a89a6c82
Donald Bull <81042354+Bullbotbam@users.noreply.github.com>
2021-09-12 13:06:26 -0500

    tst

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,10 +1,57 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+	btn: {
+		fontSize: 60,
+		backgroundColor: 'violet',
+		'&:hover': {
+			backgroundColor: 'blue',
+		},
+	},
+	title: {
+		textDecoration: 'underline',
+		marginBottom: 20,
+	},
+});
 
 export default function Create() {
+	const classes = useStyles();
+
 	return (
-		<div>
-			<Typography>Create a New Note</Typography>
-		</div>
+		<Container>
+			<Typography
+				className={classes.title}
+				variant="h6"
+				component="h2"
+				gutterBottom
+				color="textSecondary"
+			>
+				Create a New Note
+			</Typography>
+			<Button
+				className={classes.btn}
+				onClick={() => console.log('You clicked me!')}
+				type="submit"
+				color="secondary"
+				variant="contained"
+				endIcon={<KeyboardArrowRightIcon />}
+			>
+				Submit
+			</Button>
+
+			{/* icons */}
+			{/* <br />
+			<AcUnitOutlinedIcon />
+			<AcUnitOutlinedIcon color="secondary" fontSize="large" />
+			<AcUnitOutlinedIcon color="secondary" fontSize="small" />
+			<AcUnitOutlinedIcon color="action" fontSize="small" />
+			<AcUnitOutlinedIcon color="error" fontSize="small" />
+			<AcUnitOutlinedIcon color="disabled" fontSize="small" /> */}
+		</Container>
 	);
 }
